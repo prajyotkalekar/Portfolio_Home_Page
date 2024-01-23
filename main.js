0c7496f207f8ec8e62226244ed6e32c75efee6cd
prajyot <prkalekar1809@gmail.com>
2024-01-23 15:07:23 +0530

    Updated

---
+++ main.js
@@ -0,0 +1,41 @@
+// menu
+
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector("nav ul");
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+
+// Dark - Light mode        
+
+const darkModeButton = document.getElementById('darkModeButton');
+const body = document.body;
+
+const enableDarkMode = () => {
+    body.classList.add('dark-mode');
+}
+
+const disableDarkMode = () => {
+    body.classList.remove('dark-mode');
+}
+
+darkModeButton.addEventListener('change', () => {
+    if(darkModeButton.checked) {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+});
+
+// Multiple Text
+
+const typed = new Typed('.multiple', {
+    strings:['Computer Engineer','Front-End Developer','SQL Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
